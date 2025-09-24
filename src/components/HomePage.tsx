9c783c689d61c703192b02d3b92a9f72b2d85711
Nazheef Thareq <nazheefthareq15@gmail.com>
2025-09-24 15:19:57 +0700

    feat/component: navbar-menu import: font and component

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,42 @@
+"use client";
+import StaggeredMenu from "./StaggeredMenu";
+
+// Definisi komponen HomePage dan ekspor sebagai default
+export default function HomePage() {
+    // Definisi data menu di dalam komponen
+    const menuItems = [
+        { label: 'Home', ariaLabel: 'Go to home page', link: '/' },
+        { label: 'About', ariaLabel: 'Learn about us', link: '/about' },
+        { label: 'Services', ariaLabel: 'View our services', link: '/services' },
+        { label: 'Contact', ariaLabel: 'Get in touch', link: '/contact' }
+    ];
+
+    const socialItems = [
+        { label: 'Twitter', link: 'https://twitter.com' },
+        { label: 'GitHub', link: 'https://github.com' },
+        { label: 'LinkedIn', link: 'https://linkedin.com' }
+    ];
+
+    // Komponen harus mengembalikan (return) elemen JSX
+    return (
+        <nav className="md:mx-10 sm:mx-4">
+            <div style={{ height: '100vh', background: '#FFFFFF' }}>
+                <StaggeredMenu
+                    position="right"
+                    items={menuItems}
+                    socialItems={socialItems}
+                    displaySocials={true}
+                    displayItemNumbering={true}
+                    menuButtonColor="#000000"
+                    openMenuButtonColor="#000000"
+                    changeMenuColorOnOpen={true}
+                    colors={['#B19EEF', '#5227FF']}
+                    logoUrl="/images/logos/logo-icon(black-blue).png"
+                    accentColor="#4E71FF"
+                    onMenuOpen={() => console.log('Menu opened')}
+                    onMenuClose={() => console.log('Menu closed')}
+                />
+            </div>
+        </nav>
+    );
+}
